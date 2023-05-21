e7f375bfa9cd0db8d64f2576f119c2f165e18913
Aleksandr <shohin-sasha1993@yandex.ru>
2023-05-21 09:53:41 +0300

    Add project files.

---
+++ HomeWorfPart2/Program.cs
@@ -0,0 +1,63 @@
+﻿
+//1
+
+//int a = int.Parse(Console.ReadLine());
+//double b = double.Parse(Console.ReadLine());
+//Console.WriteLine("привет," + a + "!");
+//double y = a + b;
+//Console.WriteLine(a + "+" + b + "=" + y);
+//Console.WriteLine("пока," + b + "...");
+
+
+//2
+//Console.WriteLine("Введите число а - ");
+//int a = Convert.ToInt32(Console.ReadLine());//Или Convert.ToInt32 Или int.Parse
+//Console.WriteLine("Введите число b - ");
+//int b = int.Parse(Console.ReadLine());
+
+//double y = (5 * a + Math.Pow(b, 4)) / (b - a);
+//if (b - a == 0)
+//{
+//    Console.WriteLine("Дурак, на 0 делить нельзя");
+//}
+//else
+//{
+//    Console.WriteLine("Ваш ответ = " + y);
+//};
+//3
+//Console.WriteLine("Напаши слово - s1");
+//string s1 = Console.ReadLine();
+//Console.WriteLine("Напаши слово - s2");
+//string s2 = Console.ReadLine();
+//Console.WriteLine("Меняем?");
+//string d = Console.ReadLine();
+//if (d == "Да")
+//{
+//    string s3 = s1;
+
+//    s1 = s2;
+//    s2 = s1;
+
+//    Console.WriteLine("s1 =" + s1 + " и s2 =" + s2);
+//}
+//else if(d == "Нет")
+//{
+//    Console.WriteLine("s1 =" + s1 + " и s2 =" + s2);
+//}
+//else
+//{
+//    Console.WriteLine("пиши Да или Нет Мутень!!!");
+//}
+//4
+//Console.WriteLine("Введите первое число-");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите второе число-");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите третее число-");
+//int c = int.Parse(Console.ReadLine());
+//double x = (c - b) / a;
+//Console.WriteLine("Число Х равно:" + x);
+
+
+
+
